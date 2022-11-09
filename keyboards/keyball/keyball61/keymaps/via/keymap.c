df9b92da3b64ed6a3b5c805090d0b891375c9417
Gabriel Simmons <simmons.gabe@gmail.com>
2022-11-08 22:32:38 -0800

    eel-hud v1

--- keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -62,6 +62,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   // If console is enabled, it will print the matrix position and status of each key pressed
 #ifdef CONSOLE_ENABLE
     uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+    uprintf("Active layer: %u\n", biton32(layer_state));
 #endif 
   return true;
 }
@@ -75,7 +76,10 @@ void keyboard_post_init_user(void) {
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
     keyball_set_scroll_mode(get_highest_layer(state) == 3);
-    uprintf("layer%u", state);
+
+#ifdef CONSOLE_ENABLE
+    uprintf("layer%u\n", get_highest_layer(state));
+#endif 
     return state;
 }
 
